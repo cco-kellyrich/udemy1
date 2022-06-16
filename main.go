12041058c2dbc3dfe49b5828322df130df970dc6
Kelly Rich <106700938+cco-kellyrich@users.noreply.github.com>
2022-06-15 22:07:03 -0700

    added new formulas

--- main.go
+++ main.go
@@ -1,21 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
-//const p = 500
-//const t = .78
+const p float64 = 500
+const t float64 = .78
+
+//const d = 10
 
 func p1(d float64) {
+	//var p float64 = 500
+	//var t float64 = .78
 
-	var p float64 = 500
-	var t float64 = .78
 	fmt.Println("Paycheck: ", (p*d)*t)
+	fmt.Printf("What I make a month: %v if I work %v days\n", (p*d)*t*2, d*2)
+
 }
 
 func main() {
 
-	p1(10)
+	// func p1(d, float64) int{
+	// 	var p float64 = 500
+	// 	var t float64 = .78
+
+	// 	fmt.Println("Paycheck: ", (p*d)*t)
+	// }
+
+	p1(11)
+	//fmt.Println("Make a month", p1*m)
+
+	//var p float64 = 500
+	//var t float64 = .78
+	//fmt.Println("Paycheck: ", (p*d)*t)
 
 }
-
-//
